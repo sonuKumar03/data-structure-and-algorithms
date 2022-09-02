94d82dfc502c2899ad8b30120c91dc641ae75a16
Sonu Kumar <sonukumar03xt@gmail.com>
2022-09-02 23:47:55 +0530

    Add project files.

---
+++ basics/Graph/Traversal.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace basics.Graph
+{
+    public class Traversal
+    {
+        List<int>  bfs(List<List<int>> adj,ref List<bool>visited, ref List<int>storefBfs,int node)
+        {
+            visited[node] = true;
+            storefBfs.Add(node);
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(node);
+            while (queue.Count > 0)
+            {
+                int _node = queue.Dequeue();
+                foreach(var e in adj[_node])
+                {
+                    if (!visited[_node])
+                    {
+                        visited[_node] = true;
+                        queue.Enqueue(_node);
+                    }
+                }
+            }
+            return storefBfs;
+        }
+        
+       public List<int> BfsOfGraph(List<List<int>>adj,int nodes)
+        {
+            List<int> storefBfs = new List<int>();
+            List<bool> visited = new List<bool>();
+            for(int i = 1; i <= nodes; i++)
+            {
+                visited.Add(false);
+            }
+            for(int i = 1; i <nodes; i++)
+            {
+                bfs(adj,ref visited,ref storefBfs,i);
+            }
+            return storefBfs;
+        }
+
+        List<int> dfsNonRecursive(List<List<int>> adj, ref List<bool> visited,int node,ref List<int> storeDfs)
+        {
+            Stack<int> stack =new Stack<int>();
+            visited[node] = true;
+            storeDfs.Add(node);
+            stack.Push(node);
+            while (stack.Count > 0)
+            {
+                int cur = stack.Peek();
+                stack.Pop();
+                foreach(var e in adj[node])
+                {
+                    if (!visited[e])
+                    {
+                        stack.Push(e);
+                        visited[e] = true;
+                        storeDfs.Add(e);
+                    }
+                }
+            }
+            return storeDfs;
+        }
+        void dfsRecursive(List<List<int>> adj, ref List<bool> visited, int node, ref List<int> storeDfs)
+        {
+            visited[node] = true;
+            storeDfs.Add(node);
+            foreach(var e in adj[node])
+            {
+                if (!visited[e])
+                {
+                    dfsNonRecursive(adj, ref visited, e, ref storeDfs);
+                }
+            }
+        }
+        public List<int> DFSOfGraph(List<List<int>>adj,int nodes)
+        {
+            
+            List<int> storedDfs = new List<int>();
+            List<bool> visited = new List<bool>(nodes);
+            for(int i = 1; i <=nodes; i++)
+            {
+                visited.Add(false);
+            }
+            for (int i =1;i<nodes;i++)
+            {
+                if (!visited[i])
+                {
+                    dfsRecursive(adj, ref visited, i, ref storedDfs);
+                }
+            }
+            return storedDfs;
+        }
+    }
+}
---
+++ basics/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using basics.Graph;
+using System.IO;
+namespace basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFilePath = @"C:\Users\Sonu\source\repos\basics\basics\input.txt";
+            int edges = int.Parse(File.ReadAllLines(inputFilePath)[0].Split(" ")[0])+1;
+            int vertices =int.Parse(File.ReadAllLines(inputFilePath)[0].Split(" ")[1])+1;
+
+            List<List<int>> adj = new List<List<int>>(edges);
+            for(int i = 1; i <= edges; i++)
+            {
+                adj.Add(new List<int>());
+            }
+            foreach(var line in File.ReadLines(inputFilePath))
+            {
+                var str = line.Split(" ");
+                int v = int.Parse(str[0]);
+                int u = int.Parse(str[1]);
+                adj[u].Add(v);
+                adj[v].Add(u);
+            }
+
+            Traversal traversal = new Traversal();
+            List<int> storefDfsNonRec = traversal.BfsOfGraph(adj, vertices);
+            foreach (var t in storefDfsNonRec)
+            {
+                Console.Write(t + ",");
+            }
+            Console.WriteLine();
+           
+        }
+    }
+}
